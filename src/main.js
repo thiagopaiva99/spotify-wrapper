5cef29e9c6b6b382fe94fc6374a58c5cc45a6307
thiagopaiva99 <thiagopaiva99@gmail.com>
2017-11-14 14:48:04 -0200

    Commit automatico do terminal/Automatic terminal commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+export const search = () => {
+
+}
+
+export const searchAlbums = () => {
+    
+}
+
+export const searchArtists = () => {
+    
+}
+
+export const searchTracks = () => {
+    
+}
+
+export const searchPlaylists = () => {
+    
+}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,26 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+    describe('smoke tests', () => {
+        it('should exist the search method', () => {
+            expect(search).to.exist;
+        });
 
+        it('should exist the search albums method', () => {
+            expect(searchAlbums).to.exist;
+        });
+
+        it('should exist the search artists method', () => {
+            expect(searchArtists).to.exist;
+        });
+
+        it('should exist the search tracks method', () => {
+            expect(searchTracks).to.exist;
+        });
+
+        it('should exist the search playlists method', () => {
+            expect(searchPlaylists).to.exist;
+        });
+    });
 });

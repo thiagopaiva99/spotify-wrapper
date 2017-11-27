b5ad71fd2998b53c63b40686db6203ff09ec37de
Thiago Galvani <thiagopaiva99@gmail.com>
2017-11-27 09:45:52 -0200

    commit changes

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-export const search = () => {
-    fetch('https://spotify.com')
+export const search = (query, type) => {
+    const code = 'BQBSadGHO1V60RViJcM-BiEh6XGbfhPDoBWQ5c_YKwzr9AR4kgx0xVCH16r9iqwjkHidQQR6CT_qeyDUtdqKXmnsmTgdBCOM8yeJFoTxJrQccCaOPOIuCtaFaQaI6iqnF1XjZhghT96mo88n1EqTQkBxa6BMc_mKBPlkkh3yN99_X_fV3NrsffQCFrjdFPpZDelgIiVWGn0D-2oYA3no_cIAWl5kxI4lPaudWFGBvv3QqQP71cw22ysrQZCIxGMkXWardX1Vz9JkkdY';
+    const complement = `access_token=${code}&token_type=bearer`;
+    
+    fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}&${complement}`)
+        .then(data => data.json());
 }
 
-export const searchAlbums = () => {
-    
+export const searchArtists = (query) => {
+    search(query, 'artist');
 }
 
-export const searchArtists = () => {
-    
+export const searchAlbums = (query) => {
+    search(query, 'album');
 }
 
-export const searchTracks = () => {
-    
+export const searchTracks = (query) => {
+    search(query, 'track')
 }
 
-export const searchPlaylists = () => {
-    
+export const searchPlaylists = (query) => {
+    search(query, 'playlist');
 }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -11,6 +11,18 @@ chai.use(sinonChai);
 sinonStubPromise(sinon);
 
 describe('Spotify Wrapper', () => {
+    let fetchedStub;
+    let promise;
+
+    beforeEach(() => {
+        fetchedStub = sinon.stub(global, 'fetch');
+        promise = fetchedStub.returnsPromise();
+    });
+
+    afterEach(() => {
+        fetchedStub.restore();
+    });
+
     describe('smoke tests', () => {
         it('should exist the search method', () => {
             expect(search).to.exist;
@@ -35,10 +47,95 @@ describe('Spotify Wrapper', () => {
 
     describe('Generic Search', () => {
         it('should call fetch function', () => {
-            const fetchedStub = sinon.stub(global, 'fetch');
             const artists = search();
 
             expect(fetch).to.have.been.calledOnce;
+        });
+
+        it('should call fetch with the correct URL', () => {
+            context('passing one type', () => {
+                const artists = search('Nirvana', 'artist');
+                expect(fetchedStub).to.have.been
+                    .calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=artist');
+
+                const albums = search('Nirvana', 'album');
+                expect(fetchedStub).to.have.been
+                    .calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=album');
+            });
+
+            context('passing more than one type', () => {
+                const artistsAndAlbums = search('Nirvana', ['artist', 'album']);
+                expect(fetchedStub).to.have.been
+                .calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=artist,album');
+            });
+        });
+
+        it('should return the JSON data from the Promise', () => {
+            promise.resolves({ body: 'json' });
+
+            const artists = search('Nirvana', 'artist');
+            expect(artists.resolveValue).to.be.eql({ body: 'json' });
+        });
+    });
+    
+    describe('Search Artists', () => {
+        it('should call fetch function', () => {
+            const artists = searchArtists('Nirvana');
+            expect(fetchedStub).to.have.been.calledOnce;
+        });
+
+        it('should call fetch with the correct URL', () => {
+            const nirvana = searchArtists('Nirvana');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=artist');
+
+            const audioslave = searchArtists('Audioslave');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=artist');
+        });
+        
+    });
+
+    describe('Search Albums', () => {
+        it('should call fetch function', () => {
+            const albums = searchAlbums('Nirvana');
+            expect(fetchedStub).to.have.been.calledOnce;
+        });
+
+        it('should call fetch with the correct URL', () => {
+            const nirvana = searchAlbums('Nirvana');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=album');
+
+            const audioslave = searchAlbums('Audioslave');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=album');
+        });
+    });
+
+    describe('Search Tracks', () => {
+        it('should call fetch function', () => {
+            const tracks = searchTracks('Nirvana');
+            expect(fetchedStub).to.have.been.calledOnce;
+        });
+
+        it('should call fetch with the correct URL', () => {
+            const nirvana = searchTracks('Nirvana');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=track');
+
+            const audioslave = searchTracks('Audioslave');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=track');
+        });
+    });
+
+    describe('Search Playlists', () => {
+        it('should call fetch function', () => {
+            const playlists = searchPlaylists('Nirvana');
+            expect(fetchedStub).to.have.been.calledOnce;
+        });
+
+        it('should call fetch with the correct URL', () => {
+            const nirvana = searchPlaylists('Nirvana');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Nirvana&type=playlist');
+
+            const audioslave = searchPlaylists('Audioslave');
+            expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Audioslave&type=playlist');
         });
     });
 });
